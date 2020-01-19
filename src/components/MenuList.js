3c838feda7aae6637e9786fc2e499de78ac0e053
Mateusz Banaś <m.banas@createit.pl>
2020-01-19 13:45:09 +0100

    Expandable menu items

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -7,6 +7,10 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { connect } from "react-redux";
 import { getMenuData } from "../actions/index";
 import {useHistory} from "react-router-dom";
@@ -36,26 +40,39 @@ const MenuList = ({ menuItems }) => {
         <List className={classes.root}>
             {menuItems.map((menuItem) => {
                 return (<ListItem key={menuItem.id} alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt={menuItem.name} src={menuItem.imgUrl} />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary={menuItem.name}
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            color="textPrimary"
-                                            className={classes.listItemTitle}
-                                        >
-                                            {menuItem.name}
-                                        </Typography>
-                                        {menuItem.price}
-                                    </React.Fragment>
-                                }
-                            />
-                            <Divider variant="inset" component="li" />
+                            <ExpansionPanel>
+                                <ExpansionPanelSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <ListItemAvatar>
+                                        <Avatar alt={menuItem.name} src={menuItem.imgUrl} />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={menuItem.name}
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="textPrimary"
+                                                    className={classes.listItemTitle}
+                                                >
+                                                    {menuItem.name}
+                                                </Typography>
+                                                {menuItem.price/100} zł
+                                            </React.Fragment>
+                                        }
+                                    />
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                        sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
                         </ListItem>
                     )
             })}
