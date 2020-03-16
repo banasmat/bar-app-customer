e411aaa80274eb55845656cbf0604008b9175e87
Mateusz Banaś <m.banas@createit.pl>
2020-03-16 20:49:32 +0100

    Vibration on status ready

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,8 +5,15 @@ import {
     ORDER_PLACED,
     ORDER_STATUS_CHECKED
 } from "../constants/action-types";
-import {ORDER_STATUS_ACCEPTED, ORDER_STATUS_FINISHED, ORDER_STATUS_NONE} from "../constants/order-status";
+import {
+    ORDER_STATUS_ACCEPTED,
+    ORDER_STATUS_FINISHED,
+    ORDER_STATUS_NONE,
+    ORDER_STATUS_READY
+} from "../constants/order-status";
 import {loadState, saveState} from "../store/localStorage";
+
+let vibrateInterval;
 
 const initialState = loadState() || {
     remotePlaces: [],
@@ -78,6 +85,21 @@ function rootReducer(state = initialState, action) {
                         priceTotal: 0
                     },
                     orderStatus: state.orderStatus = action.payload.status
+                }
+            } else if(action.payload.status === ORDER_STATUS_READY){
+                //TODO might move vibration to middleware
+                //TODO button to stop
+                vibrateInterval = setInterval(function() {
+                    window.navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+                }, 5000);
+
+            } else if(action.payload.status === ORDER_STATUS_FINISHED){
+                if(vibrateInterval) clearInterval(vibrateInterval);
+                window.navigator.vibrate(0);
+                return {
+                    ...state,
+                    currentOrderId: null,
+                    orderStatus: ORDER_STATUS_NONE
                 }
             }
 
